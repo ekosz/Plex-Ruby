57e3e2ff14fc6ad0d20ea1d47cee741b9468d773
Alex Koch <alex549us3@gmail.com>
2019-04-29 12:04:14 -0600

    fixing assert_nil deprecation warning

--- test/test_config.rb
+++ test/test_config.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 describe Plex::Config do
 
   it "should be configurable" do
-    Plex.config.auth_token.must_equal nil
+    Plex.config.auth_token.assert_nil
 
     Plex.configure do |config|
       config.auth_token = "ABCD"
